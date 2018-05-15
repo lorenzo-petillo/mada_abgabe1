3a79bd6e10aeb90a1f07913d8644bb5d8957ab8b
You <lorenzo.petillo@students.fhnw.ch>
2018-05-15 17:37:11 +0200

    added comment to Phi class

--- RSA/src/Phi.java
+++ RSA/src/Phi.java
@@ -3,6 +3,8 @@ import java.math.BigInteger;
 public class Phi {
 
     // φ(p*q) = (p-1)(q-1) -->
+    // Wir können die Primfaktorisierung überspringen, da wir diese nur aufgrund unseren mikrigen Menschenhirne brauchen
+
     public BigInteger calcPhi(BigInteger q, BigInteger p){
         BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 
