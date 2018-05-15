1511b3adc5b5ee87f7c219be48ab41f72d69e2e5
You <lorenzo.petillo@students.fhnw.ch>
2018-05-15 17:30:52 +0200

    Phi done

--- RSA/src/Phi.java
+++ RSA/src/Phi.java
@@ -2,8 +2,11 @@ import java.math.BigInteger;
 
 public class Phi {
 
-    public BigInteger getPhi(BigInteger q){
+    // φ(p*q) = (p-1)(q-1) -->
+    public BigInteger calcPhi(BigInteger q, BigInteger p){
+        BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 
+        return phi;
     }
 
 
